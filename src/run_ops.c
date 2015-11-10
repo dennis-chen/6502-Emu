3ed058d5b302a58c88b54c7e68d0661d5eb08269
Pratool Gadtaula <pratool.gadtaula@students.olin.edu>
2015-11-10 11:56:46 -0500

    added operand access when reading through hex dump file

--- src/run_ops.c
+++ src/run_ops.c
@@ -2,50 +2,57 @@
 #include "opcodes.h"
 #include "exec.h"
 
-void check_op(uint8_t op) {
-    switch (op) {
-        case ADC_IMM:
-            printf("adc imm\n");
-            break;
-        case ADC_ZP:
-            printf("adc zp\n");
-            break;
-        case LDX_IMM:
-            printf("ldx imm\n");
-            break;
-        case STX_ZP:
-            printf("stx zp\n");
-            break;
-        case SEC:
-            printf("SEC\n");
-            break;
-        case TYA:
-            printf("TYA\n");
-            break;
-        case TAY:
-            printf("TAY\n");
-            break;
-        case LDA_IMM:
-            printf("LDA imm\n");
-            break;
-        case STA_ZP:
-            printf("sta zp\n");
-            break;
-        case LDX_ZP:
-            printf("ldx zp\n");
-            break;
-        case DEY:
-            printf("dey\n");
-            break;
-        case BNE_REL:
-            printf("bne rel\n");
-            break;
-        case SBC_ZPX:
-            printf("sbc zpx\n");
-            break;
-        default:
-            printf("op code probably not declared yet\n");
-            break;
+void check_op(uint8_t *mem, int16_t start, int16_t end) {
+    int16_t pc = start;
+    while (pc <= end) {
+        switch (mem[pc]) {
+            case ADC_IMM:
+                printf("adc imm %x\n", mem[++pc]);
+                break;
+            case ADC_ZP:
+                printf("adc zp\n");
+                break;
+            case LDX_IMM:
+                printf("ldx imm %x\n", mem[++pc]);
+                break;
+            case STX_ZP:
+                printf("stx zp %x\n", mem[++pc]);
+                break;
+            case LDY_IMM:
+                printf("ldy imm %x\n", mem[++pc]);
+                break;
+            case SEC:
+                printf("SEC\n");
+                break;
+            case TYA:
+                printf("TYA\n");
+                break;
+            case TAY:
+                printf("TAY\n");
+                break;
+            case LDA_IMM:
+                printf("LDA imm\n");
+                break;
+            case STA_ZP:
+                printf("sta zp\n");
+                break;
+            case LDX_ZP:
+                printf("ldx zp\n");
+                break;
+            case DEY:
+                printf("dey\n");
+                break;
+            case BNE_REL:
+                printf("bne rel\n");
+                break;
+            case SBC_ZPX:
+                printf("sbc zpx\n");
+                break;
+            default:
+                printf("op code probably not declared yet\n");
+                break;
+        }
+        pc++;
     }
 }
 
@@ -57,8 +64,9 @@ int main(int argc, char **argv) {
     if (start == end)
         return 1;
 
-    for (int i = start; i < end; i++)
-        check_op(mem[i]);
+    //for (int i = start; i < end; i++)
+        //check_op(mem[i]);
+    check_op(mem, start, end);
     printf("\n");
 
     return 0;
